611ef8ea83a32a656ebcb5e193d0a1a9eb623316
chiraggodha <godhachirag@gmail.com>
2025-11-09 17:47:46 -0500

    pingpong passed all tests

---
+++ lab1/xv6-labs-2021/user/pingpong.c
@@ -0,0 +1,33 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+    int p[2];   // pipe: p[0] = read end, p[1] = write end
+    pipe(p);
+
+    int pid = fork();
+
+    if (pid < 0) {
+        fprintf(2, "fork failed\n");
+        exit(1);
+    } 
+    else if (pid == 0) {
+        // Child process
+        char buf[4];
+        read(p[0], buf, 4);  
+        printf("%d: received ping\n", getpid());
+        write(p[1], "pong", 4); 
+    } 
+    else {
+        // Parent process
+        write(p[1], "ping", 4); 
+        char buf[4];
+        read(p[0], buf, 4);    
+        printf("%d: received pong\n", getpid());
+    }
+
+    exit(0);
+}
